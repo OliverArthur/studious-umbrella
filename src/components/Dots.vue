827a6536dc10e6bb68f635b1ada365cbd96084b5
Oliver Arthur <contact@oliverarthurnardi.com>
2020-09-21 09:54:24 +0200

    create the dots indicators components

---
+++ src/components/Dots.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="dot">
+    <li
+      v-for="(_, index) in dots"
+      class="dot__item"
+      :class="{'dot__item--active': currentIndexEl === index}"
+      :key="index">
+      <button
+        class="button dot__button"
+        :disabled="currentIndexEl === index"
+        @click="showCardElement(index)">
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'AppDot',
+  props: {
+    dots: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
+    currentIndexEl: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    showCardElement: {
+      type: Function,
+      required: true,
+      default: () => {}
+    }
+  }
+}
+</script>

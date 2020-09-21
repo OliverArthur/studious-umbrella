56f50e6e8c7f3bf5e18b5b88f5d5e910ff02dae7
Oliver Arthur <contact@oliverarthurnardi.com>
2020-09-21 09:55:24 +0200

    create the Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,54 @@
+<template>
+<div :class="modifier" class="search">
+  <h1>Movies finder</h1>
+  <form class="search__form" role="search">
+    <label for="search">Search for your favorite movies</label>
+    <input
+      id="search"
+      placeholder="Search by movie title"
+      autofocus
+      v-model="query"
+      class="search__control"
+      type="search"
+      required
+    />
+    <button
+      class="search__button"
+      @click.prevent="gotToPage">
+      <span>GO!</span>
+    </button>
+  </form>
+  <div class="search__message">
+    <small class="info">Press enter or the button to search</small>
+  </div>
+</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import router from '@/router'
+
+export default {
+  name: 'AppSearch',
+  props: {
+    modifier: {
+      type: String
+    }
+  },
+  setup () {
+    const query = ref('')
+
+    function gotToPage () {
+      router.push({
+        name: 'result',
+        params: { query: query.value }
+      })
+    }
+
+    return {
+      query,
+      gotToPage
+    }
+  }
+}
+</script>
